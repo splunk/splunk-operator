f5df3bccb4cdd0b3536aa72facd69f0ef4cfbf15
Talha Malik <tmalik@splunk.com>
2018-10-16 16:53:58 -0700

    Use public repo for operator

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@
 
 kubectl delete splunkinstances.splunk-instance.splunk.com --all
 kubectl delete deploy splunk-operator
-operator-sdk build repo.splunk.com/tmalik/test
-docker push repo.splunk.com/tmalik/test
+operator-sdk build tmaliksplunk/test
+docker push tmaliksplunk/test
 kubectl create -f deploy/operator.yaml
