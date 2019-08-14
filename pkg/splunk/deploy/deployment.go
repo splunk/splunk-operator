4f309daf9e644a3eaec78964357f9976556e73de
Mike Dickey <mdickey@splunk.com>
2019-08-14 15:43:44 -0700

    Switched splunk image in operator.yaml to default to splunk/splunk:edge-redhat
    Switched single instances, deployer, cluster master and license master from
    using Deployment to using StatefulSet. General best practices suggest that
    Deployment should only be used for things that do not need to persist state,
    and StatefulSet should be used by everything else.
    
    This also solves the problems with PVC failures in multi-zone clusters.
    While PVCs need to be created individually outside of a Deployment, the
    PVC templates are embedded within StatefulSets. This allows the scheduler
    to ensure that volumes are provisioned according to the rules set for the
    Pods rather than independent of those constraints. Most notably, it will
    ensure that PVCs for any given pod are all created within the same zone,
    for multi-zone clusters.

--- pkg/splunk/deploy/deployment.go
+++ pkg/splunk/deploy/deployment.go
@@ -5,55 +5,12 @@
 package deploy
 
 import (
-	"fmt"
-
 	"git.splunk.com/splunk-operator/pkg/apis/enterprise/v1alpha1"
-	"git.splunk.com/splunk-operator/pkg/splunk/enterprise"
-	"git.splunk.com/splunk-operator/pkg/splunk/resources"
 	"git.splunk.com/splunk-operator/pkg/splunk/spark"
 
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-
-// CreateSplunkDeployment creates a Kubernetes Deployment for a given type of Splunk Enterprise instance.
-func CreateSplunkDeployment(cr *v1alpha1.SplunkEnterprise, client client.Client, instanceType enterprise.SplunkInstanceType, identifier string, replicas int, envVariables []corev1.EnvVar) error {
-
-	labels := enterprise.GetSplunkAppLabels(identifier, instanceType.ToString())
-	deploymentName := enterprise.GetSplunkDeploymentName(instanceType, identifier)
-
-	// get generic list of splunk persistent volume claims
-	volumeClaims, err := enterprise.GetSplunkVolumeClaims(cr, instanceType, labels)
-	if err != nil {
-		return err
-	}
-
-	// inject deployment name into the volume claim names to make them unique
-	for idx, _ := range volumeClaims {
-		volumeClaim := volumeClaims[idx]
-		volumeClaim.ObjectMeta.Name = fmt.Sprintf("pvc-%s-%s", volumeClaim.ObjectMeta.Name, deploymentName)
-		volumeClaim.SetOwnerReferences(append(volumeClaim.GetOwnerReferences(), resources.AsOwner(cr)))
-		err = CreateResource(client, &volumeClaim)
-		if err != nil {
-			return err
-		}
-	}
-
-	// get configuration for kubernetes deployment resource
-	deployment, err := enterprise.GetSplunkDeployment(cr, instanceType, deploymentName, labels, replicas, envVariables)
-	if err != nil {
-		return err
-	}
-
-	// create kubernetes deployment resource
-	err = CreateResource(client, deployment)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 
 // CreateSparkDeployment creates a Kubernetes Deployment for a given type of Spark instance.
