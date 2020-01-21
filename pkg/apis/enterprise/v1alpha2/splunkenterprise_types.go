d83e05c7d30b782217c328f287bf2d99bb0bc632
Mike Dickey <mdickey@splunk.com>
2020-01-21 10:42:24 -0800

    Using kubebuilder to generate SplunkEnterprise CRD and include OpenAPI details

--- pkg/apis/enterprise/v1alpha2/splunkenterprise_types.go
+++ pkg/apis/enterprise/v1alpha2/splunkenterprise_types.go
@@ -19,64 +19,156 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+// see also https://book.kubebuilder.io/reference/markers/crd.html
 
 // SplunkResourcesSpec defines the resource requirements for a SplunkEnterprise deployment
 type SplunkResourcesSpec struct {
-	SplunkCPURequest     string `json:"splunkCpuRequest"`
-	SparkCPURequest      string `json:"sparkCpuRequest"`
-	SplunkMemoryRequest  string `json:"splunkMemoryRequest"`
-	SparkMemoryRequest   string `json:"sparkMemoryRequest"`
-	SplunkCPULimit       string `json:"splunkCpuLimit"`
-	SparkCPULimit        string `json:"sparkCpuLimit"`
-	SplunkMemoryLimit    string `json:"splunkMemoryLimit"`
-	SparkMemoryLimit     string `json:"sparkMemoryLimit"`
-	SplunkEtcStorage     string `json:"splunkEtcStorage"`
-	SplunkVarStorage     string `json:"splunkVarStorage"`
+	// Sets the CPU request (minimum) for Splunk pods (default=”0.1”)
+	SplunkCPURequest string `json:"splunkCpuRequest"`
+
+	// Sets the CPU request (minimum) for Spark pods (default=”0.1”)
+	SparkCPURequest string `json:"sparkCpuRequest"`
+
+	// Sets the memory request (minimum) for Splunk pods (default=”1Gi”)
+	SplunkMemoryRequest string `json:"splunkMemoryRequest"`
+
+	// Sets the memory request (minimum) for Spark pods (default=”1Gi”)
+	SparkMemoryRequest string `json:"sparkMemoryRequest"`
+
+	// Sets the CPU limit (maximum) for Splunk pods (default=”4”)
+	SplunkCPULimit string `json:"splunkCpuLimit"`
+
+	// Sets the CPU limit (maximum) for Spark pods (default=”4”)
+	SparkCPULimit string `json:"sparkCpuLimit"`
+
+	// Sets the memory limit (maximum) for Splunk pods (default=”8Gi”)
+	SplunkMemoryLimit string `json:"splunkMemoryLimit"`
+
+	// Sets the memory limit (maximum) for Spark pods (default=”8Gi”)
+	SparkMemoryLimit string `json:"sparkMemoryLimit"`
+
+	// Storage capacity to request for Splunk etc volume claims (default=”1Gi”)
+	SplunkEtcStorage string `json:"splunkEtcStorage"`
+
+	// Storage capacity to request for Splunk var volume claims (default=”50Gi”)
+	SplunkVarStorage string `json:"splunkVarStorage"`
+
+	// Storage capacity to request for Splunk var volume claims on indexers (default=”200Gi”)
 	SplunkIndexerStorage string `json:"splunkIndexerStorage"`
 }
 
 // SplunkTopologySpec defines the topology for a SplunkEnterprise deployment
 type SplunkTopologySpec struct {
-	Standalones  int `json:"standalones"`
-	Indexers     int `json:"indexers"`
-	SearchHeads  int `json:"searchHeads"`
+	// The number of standalone instances to deploy.
+	// +kubebuilder:validation:Minimum=0
+	Standalones int `json:"standalones"`
+
+	// The number of indexers to deploy. If this number is greater than 0, a cluster master will also be deployed.
+	// +kubebuilder:validation:Minimum=0
+	Indexers int `json:"indexers"`
+
+	// The number of search heads to deploy. If this number is greater than 0, a deployer will also be deployed.
+	// +kubebuilder:validation:Minimum=0
+	SearchHeads int `json:"searchHeads"`
+
+	// The number of spark workers to launch (defaults to 0 if enableDFS is false, or 1 if enableDFS is true)
+	// +kubebuilder:validation:Minimum=0
 	SparkWorkers int `json:"sparkWorkers"`
 }
 
 // SplunkEnterpriseSpec defines the desired state of SplunkEnterprise
 type SplunkEnterpriseSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	EnableDFS        bool                `json:"enableDFS"`
-	SparkImage       string              `json:"sparkImage"`
-	SplunkImage      string              `json:"splunkImage"`
-	SplunkVolumes    []v1.Volume         `json:"splunkVolumes"`
-	DefaultsURL      string              `json:"defaultsUrl"`
-	LicenseURL       string              `json:"licenseUrl"`
-	ImagePullPolicy  string              `json:"imagePullPolicy"`
-	StorageClassName string              `json:"storageClassName"`
-	SchedulerName    string              `json:"schedulerName"`
-	Affinity         *v1.Affinity        `json:"affinity"`
-	Resources        SplunkResourcesSpec `json:"resources"`
-	Topology         SplunkTopologySpec  `json:"topology"`
-	Defaults         string              `json:"defaults"`
+	// If this is true, DFS will be installed and enabled on all searchHeads and a spark cluster will be created.
+	EnableDFS bool `json:"enableDFS"`
+
+	// Docker image to use for Spark instances (overrides SPARK_IMAGE environment variables)
+	SparkImage string `json:"sparkImage"`
+
+	// Docker image to use for Splunk instances (overrides SPLUNK_IMAGE environment variables)
+	SplunkImage string `json:"splunkImage"`
+
+	// List of one or more Kubernetes volumes. These will be mounted in all Splunk containers as as /mnt/<name>
+	SplunkVolumes []v1.Volume `json:"splunkVolumes"`
+
+	// Inline map of default.yml overrides used to initialize the environment
+	Defaults string `json:"defaults"`
+
+	// Full path or URL for one or more default.yml files, separated by commas
+	DefaultsURL string `json:"defaultsUrl"`
+
+	// Full path or URL for a Splunk Enterprise license file
+	LicenseURL string `json:"licenseUrl"`
+
+	// Sets pull policy for all images (either “Always” or the default: “IfNotPresent”)
+	// +kubebuilder:validation:Enum=Always;IfNotPresent
+	ImagePullPolicy string `json:"imagePullPolicy"`
+
+	// Name of StorageClass to use for persistent volume claims
+	StorageClassName string `json:"storageClassName"`
+
+	// Name of Scheduler to use for pod placement (defaults to “default-scheduler”)
+	SchedulerName string `json:"schedulerName"`
+
+	// Kubernetes Affinity rules that control how pods are assigned to particular nodes
+	Affinity *v1.Affinity `json:"affinity"`
+
+	// resource requirements for a SplunkEnterprise deployment
+	Resources SplunkResourcesSpec `json:"resources"`
+
+	// desired topology for a SplunkEnterprise deployment
+	Topology SplunkTopologySpec `json:"topology"`
 }
 
 // SplunkEnterpriseStatus defines the observed state of SplunkEnterprise
 type SplunkEnterpriseStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// current phase of the search head cluster
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	SearchPhase string `json:"searchPhase"`
+
+	// current phase of the indexer cluster
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	IndexingPhase string `json:"indexingPhase"`
+
+	// current phase of the dfs workers
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	DfsPhase string `json:"dfsPhase"`
+
+	// current phase of the standalone instances
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	StandalonePhase string `json:"standalonePhase"`
+
+	// current phase of the license master
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	LicenseMasterPhase string `json:"licenseMasterPhase"`
+
+	// current phase of the cluster master
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	ClusterMasterPhase string `json:"clusterMasterPhase"`
+
+	// current phase of the deployer
+	// +kubebuilder:validation:Enum=pending;ready;scaleup;scaledown;updating
+	DeployerPhase string `json:"deployerPhase"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // SplunkEnterprise is the Schema for the splunkenterprises API
+//
+// NAME   SEARCH        INDEXING        DFS             STANDALONE        LM      CM      DEP
+// demo   ready (5/5)   scaleup (3/5)   pending (3/5)   scaledown (5/3)   ready   ready   ready
+//
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:path=splunkenterprises,scope=Namespaced
+// +kubebuilder:resource:path=splunkenterprises,scope=Namespaced,shortName=enterprise;se
+// +kubebuilder:printcolumn:name="Search",type="integer",JSONPath=".spec.status.searchPhase",description="Status of search head cluster"
+// +kubebuilder:printcolumn:name="Indexing",type="integer",JSONPath=".spec.status.indexingPhase",description="Status of indexer cluster"
+// +kubebuilder:printcolumn:name="DFS",type="integer",JSONPath=".spec.topology.dfsPhase",description="Status of DFS spark workers"
+// +kubebuilder:printcolumn:name="Standalone",type="integer",JSONPath=".spec.topology.standalonePhase",description="Status of standlone instances"
+// +kubebuilder:printcolumn:name="LM",type="string",JSONPath=".status.licenseMasterPhase",description="Status of license master"
+// +kubebuilder:printcolumn:name="CM",type="string",JSONPath=".status.clusterMasterPhase",description="Status of cluster master"
+// +kubebuilder:printcolumn:name="Dep",type="string",JSONPath=".status.deployerPhase",description="Status of deployer"
 type SplunkEnterprise struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
