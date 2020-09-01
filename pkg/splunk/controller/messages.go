d3b0a0c00050afcbc18a15be09b18e8b0998c19e
akondur <akondur@splunk.com>
2020-08-31 18:38:14 -0700

    Fix CSPL_300 - update secret scenario and added unit testing

--- pkg/splunk/controller/messages.go
+++ pkg/splunk/controller/messages.go
@@ -12,18 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package enterprise
+package controller
 
 const (
-	// Less than zero version error
-	lessThanOrEqualToZeroVersionError = "Versions shouldn't be <= 0"
-
-	// Non-integer version error
-	nonIntegerVersionError = "Failed to convert non integer string to integer value"
-
-	// Non-matching string error
-	nonMatchingStringError = "Non-matching string secretName %s versionedSecretIdentifier %s"
-
-	// Missing Token error
-	missingTokenError = "Couldn't convert to splunk readable format, %s token missing"
+	invalidSecretObjectError = "Invalid secret object"
 )
