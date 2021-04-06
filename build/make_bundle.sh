7cd8a71cd066e4c5cf5ac380162b1e7ed019285a
Victor Ebken <vebken@splunk.com>
2021-04-06 16:17:31 -0700

    CSPL-612 upgrade operator sdk
    Upgrade operator sdk to v0.18.2

--- build/make_bundle.sh
+++ build/make_bundle.sh
@@ -126,7 +126,7 @@ cat << EOF >$YAML_SCRIPT_FILE
 EOF
 
 echo Updating $OLM_CATALOG
-operator-sdk generate csv --csv-version $VERSION --operator-name splunk --update-crds --verbose
+operator-sdk generate csv --csv-version $VERSION --operator-name splunk --update-crds --make-manifests=false --verbose
 yq w -i -s $YAML_SCRIPT_FILE $OLM_CATALOG/splunk/$VERSION/splunk.v${VERSION}.clusterserviceversion.yaml
 rm -f $YAML_SCRIPT_FILE
 
