1969636d6304def5f6e16483c06f571ff50c1724
tpereirasplunk <77701860+tpereirasplunk@users.noreply.github.com>
2021-06-09 17:57:11 -0700

    CSPL-1069: Automation for app framework phase 2 (#357)

--- test/testenv/appframework_utils.go
+++ test/testenv/appframework_utils.go
@@ -1,7 +1,11 @@
 package testenv
 
 import (
+	"fmt"
+	"strings"
+
 	enterprisev1 "github.com/splunk/splunk-operator/pkg/apis/enterprise/v1"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // GenerateAppSourceSpec return AppSourceSpec struct with given values
@@ -11,4 +15,65 @@ func GenerateAppSourceSpec(appSourceName string, appSourceLocation string, appSo
 		Location:             appSourceLocation,
 		AppSourceDefaultSpec: appSourceDefaultSpec,
 	}
+}
+
+// GetPodAppStatus Get the app install status and version number
+func GetPodAppStatus(deployment *Deployment, podName string, ns string, appname string) (string, string, error) {
+	output, _ := GetPodAppInstallStatus(deployment, podName, ns, appname)
+	status := strings.Fields(output)[1]
+	version, err := GetPodInstalledAppVersion(deployment, podName, ns, appname)
+	return status, version, err
+
+}
+
+// GetPodInstalledAppVersion Get the version of the app installed on pod
+func GetPodInstalledAppVersion(deployment *Deployment, podName string, ns string, appname string) (string, error) {
+	path := "etc/apps"
+	if strings.Contains(podName, "-indexer-") {
+		path = "etc/slave-apps/_cluster/default"
+	}
+	filePath := fmt.Sprintf("/opt/splunk/%s/%s/default/app.conf", path, appname)
+
+	confline, err := GetConfLineFromPod(podName, filePath, ns, "Version", "", false)
+	if err != nil {
+		logf.Log.Error(err, "Failed to get Version from pod", "Pod Name", podName)
+		return "", err
+	}
+	version := strings.TrimSpace(strings.Split(confline, " ")[2])
+	return version, err
+
+}
+
+// GetPodAppInstallStatus Get the app install status
+func GetPodAppInstallStatus(deployment *Deployment, podName string, ns string, appname string) (string, error) {
+	stdin := fmt.Sprintf("/opt/splunk/bin/splunk display app '%s'", appname)
+	command := []string{"/bin/sh"}
+	stdout, stderr, err := deployment.PodExecCommand(podName, command, stdin, false)
+	if err != nil {
+		logf.Log.Error(err, "Failed to execute command on pod", "pod", podName, "command", command, "stdin", stdin)
+		return "", err
+	}
+	logf.Log.Info("Command executed on pod", "pod", podName, "command", command, "stdin", stdin, "stdout", stdout, "stderr", stderr)
+
+	return strings.TrimSuffix(stdout, "\n"), nil
+}
+
+// GetPodAppbtoolStatus Get the app btool status
+func GetPodAppbtoolStatus(deployment *Deployment, podName string, ns string, appname string) (string, error) {
+	stdin := fmt.Sprintf("/opt/splunk/bin/splunk btool %s --app=%s --debug", appname, appname)
+	command := []string{"/bin/sh"}
+	stdout, stderr, err := deployment.PodExecCommand(podName, command, stdin, false)
+	if err != nil {
+		logf.Log.Error(err, "Failed to execute command on pod", "pod", podName, "command", command, "stdin", stdin)
+		return "", err
+	}
+	logf.Log.Info("Command executed on pod", "pod", podName, "command", command, "stdin", stdin, "stdout", stdout, "stderr", stderr)
+
+	if len(stdout) > 0 {
+		if strings.Contains(strings.Split(stdout, "\n")[0], "Application is disabled") {
+			return "DISABLED", nil
+		}
+		return "ENABLED", nil
+	}
+	return "", err
 }
