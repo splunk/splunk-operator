020a2c482f8627827197c77900f8f43d9aa23477
Vivek Reddy <vivekr@splunk.com>
2021-12-01 08:15:00 -0800

    removed api directory from pkg and regenerated the code

---
+++ api/v1alpha3/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha3 contains API Schema definitions for the enterprise v1alpha3 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=enterprise.splunk.com
+package v1alpha3
