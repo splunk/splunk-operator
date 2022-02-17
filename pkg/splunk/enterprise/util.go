dc8c73bd0b7d28f4f15afdb58647474698ed5f1e
Param Dhanoya <pdhanoya@splunk.com>
2022-02-17 10:57:29 -0800

    CSPL-1632-chaneged home dir to appmount due to write permission issue for splunk user

--- pkg/splunk/enterprise/util.go
+++ pkg/splunk/enterprise/util.go
@@ -892,7 +892,12 @@ func setupAppInitContainers(client splcommon.ControllerClient, cr splcommon.Meta
 			appSrcScope := getAppSrcScope(appFrameworkConfig, appSrc.Name)
 			initContainerName := strings.ToLower(fmt.Sprintf(initContainerTemplate, appSrcName, i, appSrcScope))
 
-			initEnv := []corev1.EnvVar{}
+			initEnv := []corev1.EnvVar{
+				{
+					Name:  "HOME",
+					Value: appBktMnt,
+				},
+			}
 			if appSecretRef != "" {
 				initEnv = append(initEnv, corev1.EnvVar{
 					Name: "AWS_ACCESS_KEY_ID",
