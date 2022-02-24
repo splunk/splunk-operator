7f3101f3300efc5a75e14da371c6ed394a417c83
vivek reddy <vivekr@splunk.com>
2022-02-23 19:59:38 -0800

    fixed an issue in test case for clusterwide test case

--- test/custom_resource_crud/custom_resource_crud_suite_test.go
+++ test/custom_resource_crud/custom_resource_crud_suite_test.go
@@ -49,17 +49,9 @@ func TestBasic(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	var err error
-	testenvInstance, err = testenv.NewDefaultTestEnv(testSuiteName)
-	Expect(err).ToNot(HaveOccurred())
-
-	testenvInstance, err = testenv.NewDefaultTestEnv(testSuiteName)
-	Expect(err).ToNot(HaveOccurred())
+	
 })
 
 var _ = AfterSuite(func() {
-	if testenvInstance != nil {
-		Expect(testenvInstance.Teardown()).ToNot(HaveOccurred())
-	}
 
 })
