d9919279f083da51f7137f67be7532c0b43aab7e
vivek reddy <vivekr@splunk.com>
2022-03-14 12:31:54 -0700

    ku

---
+++ docs/Upgrade.md
@@ -0,0 +1,39 @@
+=Splunk Operator Upgrade=
+
+Please note upgrading Splunk operator to Version `1.1.0` is a new installation rather than upgrade from current operator. Due to this  older Splunk operator need to be cleaned up before installing the latest version.  The script will help customer in doing these steps. This script expect current namespace where operator is installed , and path to `1.1.0` manifest file. script will 
+
+* Backup of all the operator resources within the namespace like
+** service-account, deployment, role, role-binding, cluster-role, cluster-role-binding
+* Deletes all the old Splunk operator resources and deployment
+* Installs the operator 1.1.0 in Splunk-operator namespace. 
+
+By default Splunk operator 1.1.0 will be installed to watch cluster-wide
+
+==Steps for upgrade from 1.0.5 to 1.1.0==
+
+run upgrade-to-1.1.0.sh script with below mentioned mandatory arguments 
+
+`current_namespace` current namespace where operator is installed, if its not found, it will exit with error message
+
+`manifest_file`: path where 1.1.0 Splunk operator manifest file exist
+
+===example===
+
+```upgrade-to-1.1.0.sh --current_namespace=splunk-operator manifest_file=release-v1.1.0/splunk-operator-cluster.yaml```
+
+==Configuring Operator to watch specific namespace==
+
+Edit config-map splunk-operator-config in splunk-operaor namespace, set WATCH_NAMESPACE field to the namespace operator need to watch
+```
+apiVersion: v1
+data:
+  OPERATOR_NAME: '"splunk-operator"'
+  RELATED_IMAGE_SPLUNK_ENTERPRISE: splunk/splunk:latest
+  WATCH_NAMESPACE: "add namespace here"
+kind: ConfigMap
+metadata:
+  labels:
+    name: splunk-operator
+  name: splunk-operator-config
+  namespace: splunk-operator
+```
--- tools/upgrade-to-1.1.0.sh
+++ tools/upgrade-to-1.1.0.sh
@@ -1,29 +1,56 @@
 #!/usr/bin/env bash
 
 #
-#Copyright (c) 2018-2022 Splunk Inc. All rights reserved.
+# Copyright (c) 2018-2022 Splunk Inc. All rights reserved.
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This is an upgrade script for splunk operator 
-# Version 1.1.0 is a new installation rather than upgarde for current operator 
-# Due to this user should cleanup the older script and install version 1.1.0
-# The script will help customer in doing these steps
-# This script ask current namespace where operator is installed , and 
-# * it first takes backup of all the operator resources within the namespace
-# * like serviceaccount, deployment, role, rolebinding, clusterrole, clusterrolebinding 
-# * it then deletes all the resources and installs the opeartor in splunk-operator namespace
-# * by default splunk-opeartor 1.1.0 will be installed to watch clusterwide, 
+# Please note upgrading Splunk operator to Version 1.1.0 is a new installation rather 
+# than upgrade from current operator. Due to this older Splunk operator need to be 
+# cleaned up before installing the latest version. The script will help customer in 
+# doing these steps. This script expect current namespace where operator is installed,
+# and path to 1.1.0 manifest file. script will 
+# * Backup of all the operator resources within the namespace like
+#   - service-account, deployment, role, role-binding, cluster-role, cluster-role-binding
+# * Deletes all the old Splunk operator resources and deployment
+# * Installs the operator 1.1.0 in Splunk-operator namespace. 
+# 
+# By default Splunk operator 1.1.0 will be installed to watch cluster-wide
+# Steps for upgrade from 1.0.5 to 1.1.0
+# run upgrade-to-1.1.0.sh script with below mentioned mandatory arguments 
+# current_namespace current namespace where operator is installed, if its not found, it 
+# will exit with error message
+# manifest_file: path where 1.1.0 Splunk operator manifest file exist
+#
+# example 
+# >upgrade-to-1.1.0.sh --current_namespace=splunk-operator manifest_file=release-v1.1.0/splunk-operator-cluster.yaml
+#
+#
+# Configuring Operator to watch specific namespace:
+# Edit config-map splunk-operator-config in splunk-operaor namespace, set WATCH_NAMESPACE 
+# field to the namespace operator need to watch
+#
+# apiVersion: v1
+# data:
+#   OPERATOR_NAME: '"splunk-operator"'
+#   RELATED_IMAGE_SPLUNK_ENTERPRISE: splunk/splunk:latest
+#   WATCH_NAMESPACE: "add namespace here"
+# kind: ConfigMap
+# metadata:
+#   labels:
+#     name: splunk-operator
+#   name: splunk-operator-config
+#   namespace: splunk-operator
 
 readonly CURRENT_TIME=$(date +%Y-%m-%d_%H%M-%Z)
 readonly PROGRAM_NAME=$(basename "$0")
@@ -83,6 +110,15 @@ parse_options() {
  
 }
 
+namespace_exist() {
+    echo "check if namespace ${current_namespace} exist"
+    kubectl get namespace ${current_namespace}
+    if [ $? != 0 ]
+    then
+        echo "namespace ${current_namespace} do not exist, exiting..."
+        exit 13 
+    fi
+}
 
 backup() {
     backup_file_name=backup_${CURRENT_TIME}.yaml
@@ -170,6 +206,7 @@ deploy_operator() {
 }
 
 parse_options "$@"
+namespace_exist
 backup
 delete_operator
 deploy_operator
