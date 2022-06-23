fcf03fabe282ffcc99de8a8230fa48f768175874
vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>
2022-06-23 15:39:03 -0700

    remove redirecting stderr to stdout in search head app install check
    Signed-off-by: vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>

--- pkg/splunk/enterprise/names.go
+++ pkg/splunk/enterprise/names.go
@@ -62,7 +62,7 @@ const (
 
 	manualAppUpdateCMStr = "splunk-%s-manual-app-update"
 
-	applySHCBundleCmdStr = "/opt/splunk/bin/splunk apply shcluster-bundle -target https://%s:8089 -auth admin:`cat /mnt/splunk-secrets/password` --answer-yes -push-default-apps true &> %s &"
+	applySHCBundleCmdStr = "/opt/splunk/bin/splunk apply shcluster-bundle -target https://%s:8089 -auth admin:`cat /mnt/splunk-secrets/password` --answer-yes -push-default-apps true > %s &"
 
 	shcBundlePushCompleteStr = "Bundle has been pushed successfully to all the cluster members.\n"
 
