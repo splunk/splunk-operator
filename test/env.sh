a8e82a448ecc048f22f3699fc623ce2b98057831
mgaldino-splunk <69481631+mgaldino-splunk@users.noreply.github.com>
2022-07-13 15:56:32 -0700

    Removed temporary code.

--- test/env.sh
+++ test/env.sh
@@ -2,22 +2,27 @@
 
 : "${SPLUNK_OPERATOR_IMAGE:=splunk/splunk-operator:latest}"
 : "${SPLUNK_ENTERPRISE_IMAGE:=splunk/splunk:latest}"
-: "${CLUSTER_PROVIDER:=eks}"
-: "${CLUSTER_NAME:=BiasLangScenarios}"
+: "${CLUSTER_PROVIDER:=kind}"
+: "${CLUSTER_NAME:=integration-test-cluster-eks}"
 : "${NUM_WORKERS:=3}"
 : "${NUM_NODES:=2}"
 : "${COMMIT_HASH:=}"
+# AWS specific variables
+: "${ECR_REGISTRY:=}"
+: "${VPC_PUBLIC_SUBNET_STRING:=}"
+: "${VPC_PRIVATE_SUBNET_STRING:=}"
+# Azure specific variables
+: "${AZURE_RESOURCE_GROUP:=}"
+: "${AZURE_CONTAINER_REGISTRY:=}"
+: "${AZURE_CONTAINER_REGISTRY_LOGIN_SERVER:=}"
+# Below env variables required to run license master test cases
+: "${ENTERPRISE_LICENSE_PATH:=}"
+: "${TEST_S3_BUCKET:=}"
+# Below env variables required to run remote indexes test cases
+: "${INDEXES_S3_BUCKET:=}"
+: "${AWS_S3_REGION:=}"
 # set when operator need to be installed clusterwide
 : "${CLUSTER_WIDE:=false}"
-: "${ECR_REGISTRY:=667741767953.dkr.ecr.us-west-2.amazonaws.com/mgaldino}"
-: "${VPC_PUBLIC_SUBNET_STRING:=subnet-0921cea9bcffd7b77,subnet-0dbbc27abdf4a416e,subnet-0dec6ad34b32e791f}"
-: "${VPC_PRIVATE_SUBNET_STRING:=subnet-0c068ca7d9c468e09,subnet-0b9a43cb73e3f9799,subnet-0fa22fc6046b4591f}"
-# Below env variables required to run license master test cases
-: "${ENTERPRISE_LICENSE_PATH:=/test_licenses}"
-: "${TEST_S3_BUCKET:=splk-test-data-bucket}"
-# Below env variables requried to run remote indexes test cases
-: "${INDEXES_S3_BUCKET:=splk-integration-test-bucket}"
-: "${AWS_S3_REGION:=us-west-2}"
 # Below env variable can be used to set the test cases to be run. Defaults to smoke test
 # Acceptable input is a regex matching test names
 : "${TEST_REGEX:=smoke}"
@@ -38,6 +43,14 @@ if [ -z "${PRIVATE_REGISTRY}" ]; then
           exit 1
         fi
         PRIVATE_REGISTRY="${ECR_REGISTRY}"
+        ;;
+      azure)
+        if [ -z "${AZURE_CONTAINER_REGISTRY_LOGIN_SERVER}" ]; then
+          echo "Please define AZURE_CONTAINER_REGISTRY_LOGIN_SERVER that specified where images are pushed and pulled from."
+          exit 1
+        fi
+        PRIVATE_REGISTRY="${AZURE_CONTAINER_REGISTRY_LOGIN_SERVER}"
+         echo "${PRIVATE_REGISTRY}"
         ;;
     esac
 fi
