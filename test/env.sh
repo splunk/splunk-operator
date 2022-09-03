525b8d9f6dedca60ebc094eeea1ce4874a92cd1a
Julien Ambrosiano <jambrosiano@splunk.com>
2022-09-02 19:26:02 -0700

    CSPL-1823

--- test/env.sh
+++ test/env.sh
@@ -11,20 +11,25 @@
 : "${ECR_REGISTRY:=}"
 : "${VPC_PUBLIC_SUBNET_STRING:=}"
 : "${VPC_PRIVATE_SUBNET_STRING:=}"
-# Azure specific variables
-: "${AZURE_RESOURCE_GROUP:=}"
-: "${AZURE_CONTAINER_REGISTRY:=}"
-: "${AZURE_CONTAINER_REGISTRY_LOGIN_SERVER:=}"
-: "${AZURE_CLUSTER_AGENTPOOL:=}"
-: "${AZURE_CLUSTER_AGENTPOOL_RG:=}"
-: "${AZURE_STORAGE_ACCOUNT:=}"
-: "${AZURE_MANAGED_ID_ENABLED:=}"
 # Below env variables required to run license master test cases
 : "${ENTERPRISE_LICENSE_PATH:=}"
 : "${TEST_S3_BUCKET:=}"
 # Below env variables required to run remote indexes test cases
 : "${INDEXES_S3_BUCKET:=}"
 : "${AWS_S3_REGION:=}"
+# Azure specific variables
+: "${AZURE_REGION:=}"
+: "${TEST_AZURE_CONTAINER:=}"
+: "${INDEXES_AZURE_CONTAINER:=}"
+: "${ENTERPRISE_LICENSE_AZURE_LOCATION:=}"
+: "${AZURE_RESOURCE_GROUP:=}"
+: "${AZURE_CONTAINER_REGISTRY:=}"
+: "${AZURE_CONTAINER_REGISTRY_LOGIN_SERVER:=}"
+: "${AZURE_CLUSTER_AGENTPOOL:=}"
+: "${AZURE_CLUSTER_AGENTPOOL_RG:=}"
+: "${AZURE_STORAGE_ACCOUNT:=}"
+: "${AZURE_STORAGE_ACCOUNT_KEY:=}"
+: "${AZURE_MANAGED_ID_ENABLED:=}"
 # set when operator need to be installed clusterwide
 : "${CLUSTER_WIDE:=false}"
 # Below env variable can be used to set the test cases to be run. Defaults to smoke test
