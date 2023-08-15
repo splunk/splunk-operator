c991f3639818304436791fde9a80d445861b1b8d
vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>
2023-08-15 14:59:03 -0700

    fixed an issue in cluster creation script 34
    Signed-off-by: vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>

--- test/deploy-eks-cluster.sh
+++ test/deploy-eks-cluster.sh
@@ -81,7 +81,7 @@ function createCluster() {
         }
       ]
     }"  >aws-ebs-csi-driver-trust-policy.json
-    rolename= echo ${TEST_CLUSTER_NAME} | awk -F- '{print "EBS_" $(NF-1) "_" $(NF)}'
+    rolename=$(echo ${TEST_CLUSTER_NAME} | awk -F- '{print "EBS_" $(NF-1) "_" $(NF)}')
     aws iam create-role --role-name ${rolename} --assume-role-policy-document file://aws-ebs-csi-driver-trust-policy.json --description "irsa role for ${TEST_CLUSTER_NAME}"
     aws iam attach-role-policy  --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy  --role-name ${rolename}
     kubectl annotate serviceaccount -n $namespace $service_account eks.amazonaws.com/role-arn=arn:aws:iam::$account_id:role/${rolename}
