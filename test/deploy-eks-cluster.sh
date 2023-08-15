30d83f5f61ec5c7b6988be3e6c315153486106bc
vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>
2023-08-15 14:17:23 -0700

    fixed an issue in cluster creation script 3
    Signed-off-by: vivekr-splunk <94569031+vivekr-splunk@users.noreply.github.com>

--- test/deploy-eks-cluster.sh
+++ test/deploy-eks-cluster.sh
@@ -35,7 +35,7 @@ function deleteCluster() {
     echo "Unable to delete cluster - ${TEST_CLUSTER_NAME}"
     return 1
   fi
-  rolename= echo ${TEST_CLUSTER_NAME} | awk -F: '{print "EBS_" $(NF-1) "_" $(NF)}'
+  rolename= echo ${TEST_CLUSTER_NAME} | awk -F- '{print "EBS_" $(NF-1) "_" $(NF)}'
   aws iam delete-role --role-name ${rolename}
 
   return 0
@@ -81,7 +81,7 @@ function createCluster() {
         }
       ]
     }"  >aws-ebs-csi-driver-trust-policy.json
-    rolename= echo ${TEST_CLUSTER_NAME} | awk -F: '{print "EBS_" $(NF-1) "_" $(NF)}'
+    rolename= echo ${TEST_CLUSTER_NAME} | awk -F- '{print "EBS_" $(NF-1) "_" $(NF)}'
     aws iam create-role --role-name ${rolename} --assume-role-policy-document file://aws-ebs-csi-driver-trust-policy.json --description "irsa role for ${TEST_CLUSTER_NAME}"
     aws iam attach-role-policy  --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy  --role-name ${rolename}
     kubectl annotate serviceaccount -n $namespace $service_account eks.amazonaws.com/role-arn=arn:aws:iam::$account_id:role/${rolename}
