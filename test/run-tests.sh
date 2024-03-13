4ff6249993730eb2f91c903017c739692089e76a
Arjun Kondur <akondur@splunk.com>
2024-03-13 14:11:24 -0500

    Log the Splunk Enterprise image

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -30,7 +30,9 @@ elif [  "${CLUSTER_WIDE}" != "true" ]; then
   bin/kustomize build config/crd | kubectl create -f -
 else
   echo "Installing enterprise operator from ${PRIVATE_SPLUNK_OPERATOR_IMAGE}..."
+  echo "Installing enterprise splunk image from ${SPLUNK_ENTERPRISE_IMAGE_GRAVITON}..."
   make deploy IMAGE_TAG_BASE=docker.io/splunk/splunk-operator IMG=${PRIVATE_SPLUNK_OPERATOR_IMAGE} SPLUNK_ENTERPRISE_IMAGE=${PRIVATE_SPLUNK_ENTERPRISE_IMAGE} WATCH_NAMESPACE=""
+  kubectl describe deployment/splunk-operator-controller-manager
 fi
 
 if [ $? -ne 0 ]; then
