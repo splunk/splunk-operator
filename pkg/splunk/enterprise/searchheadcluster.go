f3fb07de99d42d987f2a206b7a735f09afbff770
Arjun Kondur <akondur@splunk.com>
2024-10-01 20:19:18 -0500

    Addresses PR#1372

--- pkg/splunk/enterprise/searchheadcluster.go
+++ pkg/splunk/enterprise/searchheadcluster.go
@@ -309,7 +309,7 @@ func ApplyShcSecret(ctx context.Context, mgr *searchHeadClusterPodManager, repli
 		// Retrieve shc_secret password from Pod
 		shcSecret, err := splutil.GetSpecificSecretTokenFromPod(ctx, mgr.c, shPodName, mgr.cr.GetNamespace(), "shc_secret")
 		if err != nil {
-			return fmt.Errorf("couldn't retrieve shc_secret from secret data")
+			return fmt.Errorf("couldn't retrieve shc_secret from secret data, error: %s", err.Error())
 		}
 
 		// set the targetPodName here
@@ -320,7 +320,7 @@ func ApplyShcSecret(ctx context.Context, mgr *searchHeadClusterPodManager, repli
 		// Retrieve admin password from Pod
 		adminPwd, err := splutil.GetSpecificSecretTokenFromPod(ctx, mgr.c, shPodName, mgr.cr.GetNamespace(), "password")
 		if err != nil {
-			return fmt.Errorf("couldn't retrieve admin password from secret data")
+			return fmt.Errorf("couldn't retrieve admin password from secret data, error: %s", err.Error())
 		}
 
 		// If shc secret is different from namespace scoped secret change it
