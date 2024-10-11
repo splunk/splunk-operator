e55008caa0321eb8f94f626486b3296c17e64de3
Arjun Kondur <akondur@splunk.com>
2024-10-11 12:30:28 -0500

    Change eks cluster name

--- test/env.sh
+++ test/env.sh
@@ -3,7 +3,7 @@
 : "${SPLUNK_OPERATOR_IMAGE:=splunk/splunk-operator:latest}"
 : "${SPLUNK_ENTERPRISE_IMAGE:=splunk/splunk:latest}"
 : "${CLUSTER_PROVIDER:=eks}"
-: "${CLUSTER_NAME:=smoke-test-cicd-cluster-eks}"
+: "${CLUSTER_NAME:=smoketestcicd}"
 : "${NUM_WORKERS:=3}"
 : "${NUM_NODES:=2}"
 : "${COMMIT_HASH:=}"
