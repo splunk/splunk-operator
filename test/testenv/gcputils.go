805402011343c6a93e282048254583d2984fa66b
Vivek Reddy <vivekrsplunk@github.com>
2024-10-14 17:15:39 -0700

    adding logic to use base64 encode
    Signed-off-by: Vivek Reddy <vivekrsplunk@github.com>

--- test/testenv/gcputils.go
+++ test/testenv/gcputils.go
@@ -5,7 +5,7 @@ import (
 	"compress/gzip"
 	"context"
 	"encoding/base64"
-	"encoding/json"
+	//"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -16,7 +16,7 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/google/uuid"
-	"golang.org/x/oauth2/google"
+	//"golang.org/x/oauth2/google"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 
