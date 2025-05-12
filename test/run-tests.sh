a9218968fd0e28232d5c3ae635055cb602fef6ef
rlieberman-splunk <rlieberman@splunk.com>
2025-05-12 14:05:59 -0500

    use scriptdir to run sh files

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,5 +7,5 @@ source ${scriptdir}/env.sh
 
 PRIVATE_SPLUNK_OPERATOR_IMAGE=$(bash ${scriptdir}/get-private-registry-operator.sh | tail -1)
 PRIVATE_SPLUNK_ENTERPRISE_IMAGE=$(bash ${scriptdir}/get-private-registry-enterprise.sh | tail -1)
-./deploy-operator.sh ${PRIVATE_SPLUNK_OPERATOR_IMAGE} ${PRIVATE_SPLUNK_ENTERPRISE_IMAGE}
-./trigger-tests.sh ${PRIVATE_SPLUNK_OPERATOR_IMAGE} ${PRIVATE_SPLUNK_ENTERPRISE_IMAGE}
+bash ${scriptdir}/deploy-operator.sh ${PRIVATE_SPLUNK_OPERATOR_IMAGE} ${PRIVATE_SPLUNK_ENTERPRISE_IMAGE}
+bash ${scriptdir}/trigger-tests.sh ${PRIVATE_SPLUNK_OPERATOR_IMAGE} ${PRIVATE_SPLUNK_ENTERPRISE_IMAGE}
