ed5b41ebc893cc0c4a42e18f5687cfd330d5a542
kasiakoziol <kkoziol@splunk.com>
2025-05-15 15:31:14 +0200

    CSPL-3705 Ignoring an error if decommisioning already enabled

--- pkg/splunk/client/enterprise.go
+++ pkg/splunk/client/enterprise.go
@@ -82,8 +82,10 @@ func (c *SplunkClient) Do(request *http.Request, expectedStatus []int, obj inter
 			break
 		}
 	}
+
 	if !expectedStatusFlag {
-		return fmt.Errorf("response code=%d from %s; want %d; full response %v, response body: %v", response.StatusCode, request.URL, expectedStatus, response, response.Body)
+		respBody, _ := io.ReadAll(response.Body)
+		return fmt.Errorf("response code=%d from %s; want %d; err: %s", response.StatusCode, request.URL, expectedStatus, string(respBody))
 	}
 	if obj == nil {
 		return nil
@@ -624,7 +626,11 @@ func (c *SplunkClient) DecommissionIndexerClusterPeer(enforceCounts bool) error 
 		return err
 	}
 	expectedStatus := []int{200}
-	return c.Do(request, expectedStatus, nil)
+	err = c.Do(request, expectedStatus, nil)
+	if err!= nil && strings.Contains(err.Error(), "decommission already requested") {
+		err = nil
+	}
+	return err
 }
 
 // BundlePush pushes the Cluster manager apps bundle to all the indexer peers
