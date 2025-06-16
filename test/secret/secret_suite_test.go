1548299568d61767ea81709bb80b292c53c638bd
kasiakoziol <kkoziol@splunk.com>
2025-06-16 16:39:48 +0200

    test

--- test/secret/secret_suite_test.go
+++ test/secret/secret_suite_test.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	// PollInterval specifies the polling interval
-	PollInterval = time.Millisecond / 10
+	PollInterval = 5 * time.Second
 
 	// ConsistentPollInterval is the interval to use to consistently check a state is stable
 	ConsistentPollInterval = 200 * time.Millisecond
--- test/testenv/testenv.go
+++ test/testenv/testenv.go
@@ -52,7 +52,7 @@ const (
 	defaultTestTimeout = 1000000
 
 	// PollInterval specifies the polling interval
-	PollInterval = time.Millisecond / 10
+	PollInterval = 5 * time.Second
 
 	// ConsistentPollInterval is the interval to use to consistently check a state is stable
 	ConsistentPollInterval = 200 * time.Millisecond
