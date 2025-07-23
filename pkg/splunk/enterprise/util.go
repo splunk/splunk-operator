60d811b03a866ce4a5d92e67ea986c4458a5ebb2
rlieberman-splunk <rlieberman@splunk.com>
2025-07-23 11:20:30 -0500

    cleanup

--- pkg/splunk/enterprise/util.go
+++ pkg/splunk/enterprise/util.go
@@ -2240,7 +2240,7 @@ func updateCRStatus(ctx context.Context, client splcommon.ControllerClient, orig
 			// Status update successful
 			break
 		} else {
-			scopedLog.Error(err, "Status update failed")
+			scopedLog.Error(err, "Error trying to update the CR status")
 		}
 
 		time.Sleep(time.Duration(tryCnt) * 10 * time.Millisecond)
