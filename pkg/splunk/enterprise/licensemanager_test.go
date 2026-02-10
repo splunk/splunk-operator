2c52d4bd04f9aed3e82d41178074063621b04e69
igor.grzankowski <igorg@splunk.com>
2026-02-10 16:36:05 +0100

    debug

--- pkg/splunk/enterprise/licensemanager_test.go
+++ pkg/splunk/enterprise/licensemanager_test.go
@@ -1502,7 +1502,16 @@ func TestCheckLicenseRelatedPodFailures(t *testing.T) {
 				defer func() { newSplunkClientFunc = origFunc }()
 			}
 
+			// Diagnostic: log MockClient state before calling function
+			t.Logf("[DEBUG] MockClient Calls before: %v", c.Calls)
+			t.Logf("[DEBUG] eventPublisher: %+v", eventPublisher)
+			t.Logf("[DEBUG] fakeRecorder: %+v", fakeRecorder)
+
 			checkLicenseRelatedPodFailures(ctx, c, &lm, statefulSet, eventPublisher)
+
+			// Diagnostic: log MockClient calls after
+			t.Logf("[DEBUG] MockClient Calls after: %v", c.Calls)
+			t.Logf("[DEBUG] fakeRecorder.Events channel len: %d", len(fakeRecorder.Events))
 
 			// Check events from the fake recorder
 			if tc.expectEvent {
