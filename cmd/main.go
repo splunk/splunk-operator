0fb0e6f98a1814d09390fb877d85171707aa3b58
Kasia Koziol <kkoziol@splunk.com>
2026-02-18 16:18:47 +0100

    Fmt fix

--- cmd/main.go
+++ cmd/main.go
@@ -265,8 +265,8 @@ func main() {
 		os.Exit(1)
 	}
 	if err := (&controller.IngestorClusterReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
 		Recorder: mgr.GetEventRecorderFor("ingestorcluster-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "IngestorCluster")
