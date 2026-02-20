9189533843facb218ed8f1f28feda27921abd6a9
Gabriel J Mendoza <gabrielm@splunk.com>
2026-02-20 15:43:58 -0500

    Revert "small fix related to appframeworksS1 smoke test stability that claude was able to identify while running CI for 10.2 certification"
    This reverts commit 38298fd5725eb19b6636ffba9713ee296dfd20c6.

--- test/appframework_aws/s1/appframework_aws_test.go
+++ test/appframework_aws/s1/appframework_aws_test.go
@@ -173,7 +173,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
@@ -343,7 +342,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
@@ -521,7 +519,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
@@ -1015,7 +1012,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
@@ -1300,7 +1296,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
@@ -2048,7 +2043,6 @@ var _ = Describe("s1appfw test", func() {
 						Name: mcName,
 					},
 				},
-				Replicas:           1,
 				AppFrameworkConfig: appFrameworkSpec,
 			}
 
