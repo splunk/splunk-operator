af2a6e4ea573f10993f5ddf74c13df92b5f8c6fa
rlieberman-splunk <rlieberman@splunk.com>
2026-04-06 08:42:28 -0500

    increase timeout for test cases

--- test/appframework_aws/m4/appframework_aws_suite_test.go
+++ test/appframework_aws/m4/appframework_aws_suite_test.go
@@ -56,7 +56,7 @@ func TestBasic(t *testing.T) {
 	RegisterFailHandler(Fail)
 
 	sc, _ := GinkgoConfiguration()
-	sc.Timeout = 420 * time.Minute
+	sc.Timeout = 540 * time.Minute
 
 	RunSpecs(t, "Running "+testSuiteName, sc)
 }
