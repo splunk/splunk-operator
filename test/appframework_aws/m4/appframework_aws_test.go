23957e01ded0aa3f3e91b7cd3438f28e661ef155
Kasia Koziol <kkoziol@splunk.com>
2026-04-10 14:34:14 +0200

    CSPL-3775 Adjust timeouts

--- test/appframework_aws/m4/appframework_aws_test.go
+++ test/appframework_aws/m4/appframework_aws_test.go
@@ -1050,7 +1050,7 @@ var _ = Describe("m4appfw test", func() {
 			Expect(err).To(Succeed(), "Unable to update config map")
 
 			// Wait for Monitoring Console to reach Ready phase
-			err = testcaseEnvInst.WaitForMonitoringConsolePhase(ctx, deployment, testcaseEnvInst.GetName(), mc.Name, enterpriseApi.PhaseReady, 2*time.Minute)
+			err = testcaseEnvInst.WaitForMonitoringConsolePhase(ctx, deployment, testcaseEnvInst.GetName(), mc.Name, enterpriseApi.PhaseReady, 10*time.Minute)
 			Expect(err).To(Succeed(), "Timed out waiting for MonitoringConsole to reach Ready phase")
 
 			// Verify Monitoring Console is ready and stays in ready state
