7cc5715f9141bbbf61a7f38c57a9d9e00e14f2a7
igor.grzankowski <igorg@splunk.com>
2026-04-14 11:15:43 +0200

    up the timeout

--- test/appframework/c3/manager_appframework_test.go
+++ test/appframework/c3/manager_appframework_test.go
@@ -56,6 +56,7 @@ var _ = Describe("c3appfw test", func() {
 		name := fmt.Sprintf("%s-%s", testenvInstance.GetName(), testenv.RandomDNSName(3))
 		testcaseEnvInst, err = testenv.NewDefaultTestCaseEnv(testenvInstance.GetKubeClient(), name)
 		Expect(err).To(Succeed(), "Unable to create testcaseenv")
+		testenv.SpecifiedTestTimeout = 4000
 		deployment, err = testcaseEnvInst.NewDeployment(testenv.RandomDNSName(3))
 		Expect(err).To(Succeed(), "Unable to create deployment")
 
