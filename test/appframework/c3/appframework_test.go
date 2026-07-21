ca655cf9ab7d761cfd17acb29239a32ed68c9253
Gabriel Mendoza <gabrielm@splunk.com>
2026-07-21 01:21:50 -0400

    fix(test/appframework): widen C3 search-visibility timeout to ReadinessPollTimeout (CSPL-5069)

--- test/appframework/c3/appframework_test.go
+++ test/appframework/c3/appframework_test.go
@@ -558,7 +558,7 @@ var _ = Describe("c3appfw test", func() {
 			searchString := fmt.Sprintf("index=%s host=%s | stats count", testenv.DefaultIngestIndex, indexerName)
 			Eventually(func() (int, error) {
 				return testenv.CountSearchResults(ctx, deployment, searchPod, searchString)
-			}, 2*time.Minute, testenv.PollInterval).Should(BeNumerically(">", 0), "No data from new indexer %s became searchable on pod %s", indexerName, searchPod)
+			}, testenv.ReadinessPollTimeout, testenv.PollInterval).Should(BeNumerically(">", 0), "No data from new indexer %s became searchable on pod %s", indexerName, searchPod)
 
 			//########## SCALING UP VERIFICATIONS #########
 			_, err = testcaseEnvInst.VerifyAppFrameworkState(ctx, deployment, allAppSourceInfo, splunkPodUIDs, "")
@@ -628,12 +628,12 @@ var _ = Describe("c3appfw test", func() {
 				return testcaseEnvInst.VerifyRFSFMet(attemptCtx, deployment)
 			}, deployment.GetTimeout(), testenv.PollInterval).Should(Succeed(), "RF/SF not met")
 
-			// Search for data from removed indexer
+			// Search for data from removed indexer. See ReadinessPollTimeout rationale above.
 			searchPod = fmt.Sprintf(testenv.SearchHeadPod, deployment.GetName(), scaledSHReplicas-1)
 			searchString = fmt.Sprintf("index=%s host=%s | stats count by host", testenv.DefaultIngestIndex, indexerName)
 			Eventually(func() (int, error) {
 				return testenv.CountSearchResults(ctx, deployment, searchPod, searchString)
-			}, 2*time.Minute, testenv.PollInterval).Should(BeNumerically(">", 0), "Data from retained indexer %s was not searchable on pod %s after scale down", indexerName, searchPod)
+			}, testenv.ReadinessPollTimeout, testenv.PollInterval).Should(BeNumerically(">", 0), "Data from retained indexer %s was not searchable on pod %s after scale down", indexerName, searchPod)
 
 			//######## SCALING DOWN VERIFICATIONS #########
 			_, err = testcaseEnvInst.VerifyAppFrameworkState(ctx, deployment, allAppSourceInfo, splunkPodUIDs, "")
